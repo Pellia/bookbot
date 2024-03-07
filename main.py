27dcbe79e754fe1e85c9c2400cb5d90a92d0f625
Pellia <24740143+Pellia@users.noreply.github.com>
2024-03-07 22:03:21 +0100

    New feature: count letters in book text

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
     path_to_book = "books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
-    count = get_count_words(book_text)
+    word_count = get_count_words(book_text)
+    letter_count = get_count_letters(book_text)
     print(book_text)
-    print(f"The book contains {count} words")
+    print(f"The book contains {word_count} words")
+    print(letter_count)
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -12,5 +14,15 @@ def get_book_text(file_path):
 def get_count_words(text):
     words = text.split()
     return len(words)
+
+def get_count_letters(text):
+    letter_list = {}
+    for char in text:
+        letter = char.lower()
+        if letter in letter_list:
+            letter_list[letter] += 1
+        else:
+            letter_list[letter] = 1
+    return letter_list
 
 main()
