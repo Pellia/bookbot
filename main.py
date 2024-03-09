f60367e12f538fd5ef25d28976d7ee8a5ff7ffdd
Pellia <24740143+Pellia@users.noreply.github.com>
2024-03-09 17:50:04 +0100

    New feature: report function

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    chars_sorted_list = get_sort_dict(chars_dict)
 
     # Report
-    print("----- Begin report of books/frankenstein.txt -----")
+    print(f"----- Begin report of {book_path} -----")
     print(f"- The book contains {num_words} words -")
-    print("\n")
-    print("The book contains the following letters:")
-    print(chars_dict)
-    print("\n")
+    print("")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item["char"]}' character was found {item["num"]} times")
+    print("")
     print("----- End report -----")
 
 def get_book_text(file_path):
@@ -29,5 +32,15 @@ def get_chars_dict(text):
         else:
             chars[char] = 1
     return chars
+
+def get_sort_dict(chars_dict):
+    list_dict = []
+    for char in chars_dict:
+            list_dict.append({"char": char, "num": chars_dict[char]})
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
